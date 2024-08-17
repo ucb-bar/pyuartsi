fd2e3053fba80b148faf5248dcfd867289401d83
-T.K.- <t_k_233@outlook.com>
2024-08-16 22:41:13 -0700

    ADD: update install config

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-import setuptools
+from setuptools import setup, find_namespace_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="pyuartsi",
+setup(
+    name="barstools.uartsi",
     version="2024.08.14",
     author="-T.K.-",
     author_email="t_k_233@outlook.email",
@@ -21,9 +21,9 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "pyglet",
+        "pyserial",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src/"},
+    packages=find_namespace_packages(where="src/", include=["barstools.uartsi"]),
     python_requires=">=3.10",
 )
